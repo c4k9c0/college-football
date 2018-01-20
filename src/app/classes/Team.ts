7a18088249b7f627bb6585014d4d6d5ee101000a
Charlie Cox <coxcharl@amazon.com>
2018-01-20 17:01:53 -0600

    Improved Css

--- src/app/classes/Team.ts
+++ src/app/classes/Team.ts
@@ -1,10 +1,22 @@
 export class Team {
   name: string;
   id: number;
+  wins: number;
+  losses: number;
+  mascot: string;
   selected: boolean;
-  constructor(name: string, id: number) {
+
+  constructor(
+    name: string,
+    id: number,
+    wins: number,
+    losses: number,
+    mascot: string) {
     this.name = name;
     this.id = id;
+    this.wins = wins;
+    this.losses = losses;
+    this.mascot = mascot;
     this.selected = false;
   }
 }
